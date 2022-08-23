902e137409f7be77e9ccafc7e732ec8b5c2a7981
Svetatevs <urusel.k@mail.ru>
2022-08-24 00:42:28 +0300

    Решение задачи 12

---
+++ zadacha12/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите число: ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число: ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+int result = firstNumber / secondNumber;
+if(result == secondNumber) 
+{
+Console.Write("Кратно");
+}
+else
+{
+    double ostatok = firstNumber % secondNumber;
+    Console.Write($"Остаток: {ostatok}");
+}
