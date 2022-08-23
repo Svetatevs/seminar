f5ee571e77424bb8bd5e2e20d757399bbe9f2428
Svetatevs <urusel.k@mail.ru>
2022-08-24 00:21:19 +0300

    Решение задачи 11

---
+++ zadacha11/Program.cs
@@ -0,0 +1,7 @@
+﻿Random random = new Random();
+int RandomNumber = random.Next(100, 1000);
+Console.WriteLine(RandomNumber);
+int firstNumber = RandomNumber / 100;
+int secondNumber = RandomNumber % 10;
+Console.Write(firstNumber);
+Console.Write(secondNumber);
